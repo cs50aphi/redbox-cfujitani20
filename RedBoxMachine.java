55afde1ae4d3bfeb30985e67d7d6cc5db07a6418
cfujitani20 <cfujitani20@punahou.edu>
2020-04-10 05:40:39 +0000

    Almost finished with program, still fixing main ui as I am getting out of bounds errors

--- RedBoxMachine.java
+++ RedBoxMachine.java
@@ -12,22 +12,23 @@ import java.io.FileNotFoundException;
 public class RedBoxMachine
 {
    //Create an instance variable to hold all of the DVDs.
-
+   ArrayList<DVD> library = new ArrayList<DVD>();
+   private int num;
    /** the list of DVDs */
-   
+
    /** Constructs a Redbox Machine and fills it with DVDs
     *  Reads the file MovieList.txt so make sure that the
     *  file is in the same folder as the RedboxMachine.class
     *  file.
     */
-    
+
    public RedBoxMachine()
    {
       // Complete the constructor.
       // Leave this method. It will fill the machine with DVDs.
       fillMachine();
    }
-   
+
    /** Searches for the movie with the provided title and returns
     *  the position of the DVD in the list if the DVD is found and
     *  -1 if the DVD is not in the list.
@@ -35,13 +36,22 @@ public class RedBoxMachine
     *  @return the index of the DVD in the list if present and
     *          -1 if the DVD is not in the list.
     */
+
    public int searchForMovie(String title)
    {
+      for (int i = 0; i > library.size(); i++ )
+      {
+         if ( library.get(i).getTitle().equals(title))
+         {
+            return i;
+         }
+      }
+      return -1;
       // Complete the method to search for a movie.
       // If placement is -1, then the movie isn't there.
       // Find the index of i if the movie is there.
    }
-   
+
    /** Returns the titles of all available DVD's in
     *  the machine.
     *  @return an ArrayList of Strings containing the
@@ -49,11 +59,16 @@ public class RedBoxMachine
     */
    public ArrayList<String> getAvailableMovies()
    {
+      ArrayList<String> temp = new ArrayList<String>();
+      for (int p = 0; p > library.size(); p++)
+      {
+         temp.add(library.get(p).getTitle());
+      }
+      return temp;
       // Complete the method to get all available movie titles.
-
    }
-   
-   /** Allows a customer to rent a movie. When the movie is rented, the number 
+
+   /** Allows a customer to rent a movie. When the movie is rented, the number
     *  of available copies is reduced by 1. If there are 0 copies of the movie left
     *  after the transaction, the movie is removed from the list.
     *  @param title the title of the movie being rented.
@@ -62,20 +77,42 @@ public class RedBoxMachine
     */
    public boolean rent(String title)
    {
+      for (int l = 0; l > library.size(); l++)
+      {
+         if (library.get(l).getTitle().equals(title));
+         {
+            library.get(l).decrementCopies();
+            num = library.get(l).getNumCopies();
+            if (num == 0)
+            {
+               library.remove(library.get(l));
+            }
+            return true;
+         }
+      }
+      return false;
       // Complete the method to rent a movie.
    }
-   
-   /** Allows a customer to return a movie. When the movie is returned, the number 
-    *  of available copies is increased by 1. If the movie was not already in the 
+
+   /** Allows a customer to return a movie. When the movie is returned, the number
+    *  of available copies is increased by 1. If the movie was not already in the
     *  machine, then the DVD is added to the list.
     *  @param title the title of the movie being returned.
     *  @return the DVD that was returned by the customer.
-    */ 
+    */
    public DVD returnMovie(String title)
    {
-      // Complete the method to return a movie.
+      for (int k = 0; k < library.size(); k++)
+      {
+         if (library.get(k).getTitle().equals(title))
+         {
+            library.get(k).incrementCopies();
+            return library.get(k);
+         }
+      }
+      library.add(new DVD(title));
+      return library.get(searchForMovie(title));
    }
-   
    /** This method fills the machine with movies. You do not have
     *  to do anything to the code in this method.
     */
@@ -85,7 +122,7 @@ public class RedBoxMachine
          Scanner sn = new Scanner(new File("MovieList.txt"));
          while(sn.hasNextLine())
             returnMovie(sn.nextLine());
-              
+
       }catch(FileNotFoundException e){
          String s = "File not found! Make sure that MovieList.txt ";
          s = s + "is in the same folder as the class.";
